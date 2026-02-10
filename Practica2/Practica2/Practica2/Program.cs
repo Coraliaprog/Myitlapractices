7298365b1f65f7f67b263b8989c0680f0dca5e9a
Coraliaprog <crucetadelacruzcoralia@gmail.com>
2026-02-10 02:47:44 -0400

    Agregando mi segunda entrega

---
+++ Practica2/Practica2/Practica2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+bool continuar = true;
+while (continuar)
+{
+    Console.WriteLine("Ingrese un número entero:");
+    int numero = Convert.ToInt32(Console.ReadLine());
+
+    if (numero % 2 == 0)
+    {
+        Console.WriteLine("El número es par.");
+    }
+    else
+    {
+        Console.WriteLine("El número es impar.");
+    }
+
+    Console.WriteLine("¿Desea ingresar otro número? (s/n)");
+    string respuesta = Console.ReadLine().ToLower();
+
+    if (respuesta != "s")
+    {
+        continuar = false;
+    }
+}
